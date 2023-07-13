04cc698211c4a8d6df65a2043fcc604b357cd4d5
Nanna Ellegaard <nel@adm.ku.dk>
2023-07-13 09:21:20 +0200

    Setup

--- ext_tables.sql
+++
@@ -1,9 +0,0 @@
-#
-# Add SQL definition of database tables
-#
---
--- Table structure for table 'tt_content'
---
-CREATE TABLE tt_content (
-   tx_full_site_width VARCHAR(255) DEFAULT '' NOT NULL
-);
